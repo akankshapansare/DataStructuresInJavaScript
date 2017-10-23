5c3b1080481b1e7975b09816cdffa14673c06653
Akanksha Pansare <akankshapansare@gmail.com>
2017-10-22 20:15:09 -0700

    Insertion sort

---
+++ elementory-sort/insertionSort.js
@@ -0,0 +1,16 @@
+var insertionSort = function (array) {
+  var length = array.length;
+  
+  for(var i = 1; i < length; i++) {
+    var temp = array[i];
+    for(var j = i - 1; j >= 0 && array[j] > temp; j--) {
+      array[j+1] = array[j];
+    }
+    array[j+1] = temp;
+  }
+  
+  return array;
+};
+
+//Test the code
+insertionSort([3, 1, 9, 5]);
